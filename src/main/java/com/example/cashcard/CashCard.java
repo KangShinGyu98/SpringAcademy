1865719b76366618397b6b5f268d4a7d69c01d95
Shingyu Kang <rkdtlseb@naver.com>
2023-08-31 16:55:24 +0900

    before Lab: Implementing PUT

---
+++ src/main/java/com/example/cashcard/CashCard.java
@@ -0,0 +1,29 @@
+package com.example.cashcard;
+
+// Add this import
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+
+@Entity
+@Getter
+public class CashCard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Double amount;
+    private String owner;
+    public CashCard() {
+    }
+
+    public CashCard(Long id, Double amount, String owner) {
+        this.id = id;
+        this.amount = amount;
+        this.owner = owner;
+    }
+
+}
